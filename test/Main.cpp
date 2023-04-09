9354a6aca1d501034f8b8196cf2cfa66b1f42148
daniel <DanRhysMorris@gmail.com>
2023-04-09 12:33:39 +0100

    Fix indentation in Main.cpp

--- test/Main.cpp
+++ test/Main.cpp
@@ -8,6 +8,6 @@ using namespace Strawberry::Discord;
 
 int main(int argc, const char** argv)
 {
-    Bot bot(argv[1], Intent::GUILDS | Intent::GUILD_MEMBERS | Intent::GUILD_MESSAGES | Intent::MESSAGE_CONTENT);
-    bot.Run();
+	Bot bot(argv[1], Intent::GUILDS | Intent::GUILD_MEMBERS | Intent::GUILD_MESSAGES | Intent::MESSAGE_CONTENT);
+	bot.Run();
 }
