a4c24309042462e54c90cb89c58f5e0ba6779331
daniel <DanRhysMorris@gmail.com>
2023-04-12 15:12:17 +0100

    Guild.cpp now checks if channels field is present before attempting to parse it

--- src/Entities/Guild.cpp
+++ src/Entities/Guild.cpp
@@ -10,11 +10,14 @@ namespace Strawberry::Discord::Entity
 		guild.mGuildId = Snowflake(static_cast<std::string>(json["id"]));
 		guild.mGuildName = static_cast<std::string>(json["name"]);
 
-		for (auto channelInfo : json["channels"])
+		if (json.contains("channels"))
 		{
-			auto channel = Channel::Parse(channelInfo).Unwrap();
-			channel.mGuildId = guild.mGuildId;
-			guild.mChannels.emplace_back(std::move(channel));
+			for (auto channelInfo: json["channels"])
+			{
+				auto channel = Channel::Parse(channelInfo).Unwrap();
+				channel.mGuildId = guild.mGuildId;
+				guild.mChannels.emplace_back(std::move(channel));
+			}
 		}
 
 		return guild;
