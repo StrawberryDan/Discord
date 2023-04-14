5f513a9db9d59eda5d2e22d7c94f460192a0be32
daniel <DanRhysMorris@gmail.com>
2023-04-14 17:53:00 +0100

    Revert "Removed Socket namespace from Core::Net since it was overly verbose."
    This reverts commit 08aeafa5c7da972adcea34e22e2f0833c5cf4a2c.

--- src/Voice/Connection.cpp
+++ src/Voice/Connection.cpp
@@ -88,7 +88,6 @@ namespace Strawberry::Discord::Voice
 	void Connection::Start()
 	{
 		using nlohmann::json;
-		using namespace Core::Net;
 		using namespace Core::Net::Websocket;
 
 		// Start websocket
@@ -112,11 +111,11 @@ namespace Strawberry::Discord::Voice
 		// Receive Voice Ready
 		auto ready = wss->WaitMessage().Unwrap().AsJSON().Unwrap();
 		Core::Assert(ready["op"] == 2);
-		auto addr = IPv4Address::Parse(ready["d"]["ip"]).Unwrap();
+		auto addr = Strawberry::Core::Net::IPv4Address::Parse(ready["d"]["ip"]).Unwrap();
 		uint16_t port = ready["d"]["port"];
 		std::vector<std::string> modes = ready["d"]["modes"];
 		Core::Assert(std::find(modes.begin(), modes.end(), "xsalsa20_poly1305") != modes.end());
-		mUDP = UDPClient::Create().Unwrap();
+		mUDP = Core::Net::Socket::UDPClient::Create().Unwrap();
 
 		// Send protocol selection
 		nlohmann::json protocolSelect;
@@ -125,7 +124,7 @@ namespace Strawberry::Discord::Voice
 		protocolSelect["d"]["data"]["address"] = addr.AsString();
 		protocolSelect["d"]["data"]["port"] = port;
 		protocolSelect["d"]["data"]["mode"] = "xsalsa20_poly1305";
-		wss->SendMessage(Message(protocolSelect.dump())).Unwrap();
+		wss->SendMessage(Core::Net::Websocket::Message(protocolSelect.dump())).Unwrap();
 
 		// Receive session description
 		auto sessionDescription = wss->WaitMessage().Unwrap().AsJSON().Unwrap();
