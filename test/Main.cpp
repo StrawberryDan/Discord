2dfe006ee6fb96b66bb1e4953b566c9bd56df47a
StrawberryDan <DanRhysMorris@gmail.com>
2023-09-15 14:18:28 +0100

    Initialise networking in DiscordTest

--- test/Main.cpp
+++ test/Main.cpp
@@ -1,4 +1,5 @@
 #include "Discord/Bot.hpp"
+#include "Strawberry/Core/Net/Socket/API.hpp"
 
 
 using namespace Strawberry::Discord;
@@ -6,6 +7,8 @@ using namespace Strawberry::Discord;
 
 int main([[maybe_unused]] int argc, const char** argv)
 {
+	Strawberry::Core::Net::Socket::API::Initialise();
 	Bot bot(argv[1], Intent::GUILDS | Intent::GUILD_MEMBERS | Intent::GUILD_MESSAGES | Intent::MESSAGE_CONTENT);
 	bot.Run();
+	Strawberry::Core::Net::Socket::API::Terminate();
 }
