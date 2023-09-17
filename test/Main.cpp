4f0676c805bfbd77af5f444e63c1b4f3a2d49339
StrawberryDan <DanRhysMorris@gmail.com>
2023-09-17 16:19:05 +0100

    Use Bot::Connect in tests.

--- test/Main.cpp
+++ test/Main.cpp
@@ -8,7 +8,7 @@ using namespace Strawberry::Discord;
 int main([[maybe_unused]] int argc, const char** argv)
 {
 	Strawberry::Core::Net::Socket::API::Initialise();
-	Bot bot(argv[1], Intent::GUILDS | Intent::GUILD_MEMBERS | Intent::GUILD_MESSAGES | Intent::MESSAGE_CONTENT);
-	bot.Run();
+	auto bot = Bot::Connect(argv[1], Intent::GUILDS | Intent::GUILD_MEMBERS | Intent::GUILD_MESSAGES | Intent::MESSAGE_CONTENT);
+	bot->Run();
 	Strawberry::Core::Net::Socket::API::Terminate();
 }
